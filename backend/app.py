2e7eeb37c7fc80f40d73a17ae3ace4c1dfb20729
AnavAgrawal <anavagrawal2309@gmail.com>
2024-07-08 03:26:28 +0530

    Implemented Flask Blueprint for Products (Backend W2)

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,15 @@ from flask import Flask, request
 import psycopg2
 import dotenv
 import os
+import hashlib
+from products import products_bp
 
 # get url from .env file
 dotenv.load_dotenv()
 url = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
+app.register_blueprint(products_bp, url_prefix='/products')
 conn = psycopg2.connect(url)
 
 with conn:
@@ -19,7 +22,7 @@ with conn:
         cur.execute("CREATE TABLE IF NOT EXISTS Customer (c_ID SERIAL PRIMARY KEY, c_name VARCHAR, c_email VARCHAR, c_contact VARCHAR);")
 
         # Create Staff Table
-        cur.execute("CREATE TABLE IF NOT EXISTS Staff (s_ID SERIAL PRIMARY KEY, s_name VARCHAR, s_email VARCHAR, s_isAdmin BOOLEAN, s_contact VARCHAR);")
+        cur.execute("CREATE TABLE IF NOT EXISTS Staff (s_ID SERIAL PRIMARY KEY, s_name VARCHAR, s_email VARCHAR, s_isAdmin BOOLEAN, s_contact VARCHAR, pass_hash TEXT);")
 
         # Create Transaction Table
         cur.execute("CREATE TABLE IF NOT EXISTS Transaction (t_ID SERIAL PRIMARY KEY, c_ID INT REFERENCES Customer(c_ID), Item_SKU INT REFERENCES InventoryItem(Item_SKU), s_ID INT REFERENCES Staff(s_ID), t_Date DATE, t_Amount INT, t_Category VARCHAR);")
@@ -32,13 +35,13 @@ def get_inventory():
             inventory = cur.fetchall()
             return str(inventory)
         
-@app.post('/add_item')
-def add_item():
-    data = request.json
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("INSERT INTO InventoryItem (Item_Name, Item_Description, Item_Price, Item_Qty) VALUES (%s, %s, %s, %s);", (data['Item_Name'], data['Item_Description'], data['Item_Price'], data['Item_Qty']))
-            return (f"Item named {data['Item_Name']} added successfully")
+# @app.post('/add_item')
+# def add_item():
+#     data = request.json
+#     with conn:
+#         with conn.cursor() as cur:
+#             cur.execute("INSERT INTO InventoryItem (Item_Name, Item_Description, Item_Price, Item_Qty) VALUES (%s, %s, %s, %s);", (data['Item_Name'], data['Item_Description'], data['Item_Price'], data['Item_Qty']))
+#             return (f"Item named {data['Item_Name']} added successfully")
         
 @app.post('/add_customer')
 def add_customer():
@@ -51,9 +54,13 @@ def add_customer():
 @app.post('/add_staff')
 def add_staff():
     data = request.json
+
+    # Calculate hash of the password
+    pass_hash = hashlib.sha256(data['password'].encode()).hexdigest()
+
     with conn:
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO Staff (s_name, s_email, s_isAdmin, s_contact) VALUES (%s, %s, %s, %s);", (data['s_name'], data['s_email'], data['s_isAdmin'], data['s_contact']))
+            cur.execute("INSERT INTO Staff (s_name, s_email, s_isAdmin, s_contact, pass_hash) VALUES (%s, %s, %s, %s, %s);", (data['s_name'], data['s_email'], data['s_isAdmin'], data['s_contact'], pass_hash))
             return (f"Staff named {data['s_name']} added successfully")
         
 @app.post('/add_transaction')
@@ -61,10 +68,24 @@ def add_transaction():
     data = request.json
     with conn:
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO Transaction (c_ID, Item_SKU, s_ID, t_Date, t_Amount, t_Category) VALUES (%s, %s, %s, %s, %s, %s);", (data['c_ID'], data['Item_SKU'], data['s_ID'], data['t_Date'], data['t_Amount'], data['t_Category']))
-            cur.execute("SELECT Item_Name from InventoryItem WHERE Item_SKU = %s;", (data['Item_SKU'],))
-            item_name = cur.fetchone()
-            return (f"Transaction for item {item_name[0]} added successfully")
+
+            cur.execute("SELECT c_ID FROM Customer WHERE c_name = %s;", (data['c_name'],))
+            c_ID = cur.fetchone()[0]
+            if c_ID is None:
+                return "Customer not found"
+            
+            cur.execute("SELECT s_ID FROM Staff WHERE s_name = %s;", (data['s_name'],))
+            s_ID = cur.fetchone()[0]
+            if s_ID is None:
+                return "Staff not found"
+            
+            cur.execute("SELECT Item_SKU FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            Item_SKU = cur.fetchone()[0]
+            if Item_SKU is None:
+                return "Item not found"
+
+            cur.execute("INSERT INTO Transaction (c_ID, Item_SKU, s_ID, t_Date, t_Amount, t_Category) VALUES (%s, %s, %s, %s, %s, %s);", (c_ID, Item_SKU, s_ID, data['t_Date'], data['t_Amount'], data['t_Category']))
+            return (f"Transaction for item {data['Item_Name']} added successfully")
 
 @app.get('/total_sales')
 def get_sales():
@@ -72,7 +93,7 @@ def get_sales():
         with conn.cursor() as cur:
             cur.execute("SELECT SUM(t_Amount) FROM Transaction;")
             total_sales = cur.fetchone()
-            return f"Total sales done are Rs.{str(total_sales[0])}"
+            return f"Total sales done are Rs. {str(total_sales[0])}"
 
 
 if __name__ == '__main__':
---
+++ backend/products.py
@@ -0,0 +1,100 @@
+from flask import Blueprint, request
+import psycopg2
+import dotenv
+import os
+
+# get url from .env file
+dotenv.load_dotenv()
+url = os.getenv('DATABASE_URL')
+
+products_bp = Blueprint('products', __name__)
+conn = psycopg2.connect(url)
+
+@products_bp.get('/')
+def get_products():
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM InventoryItem;")
+            inventory = cur.fetchall()
+            items_return = "Name : Quantity<br>"
+            items_return += "<br>".join([str(item[1]) + " : " + str(item[4]) for item in inventory])
+            return items_return
+        
+@products_bp.post('/add_item')
+def add_item():
+    data = request.json
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO InventoryItem (Item_Name, Item_Description, Item_Price, Item_Qty) VALUES (%s, %s, %s, %s);", (data['Item_Name'], data['Item_Description'], data['Item_Price'], data['Item_Qty']))
+            return (f"Item named {data['Item_Name']} added successfully")  
+
+# Delete item from inventory
+@products_bp.post('/delete_item')
+def delete_item():
+    data = request.json
+    with conn:
+        with conn.cursor() as cur:
+            
+            # Check if item exists
+            cur.execute("SELECT * FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            item = cur.fetchall()
+            if len(item) == 0:
+                return (f"Item with name {data['Item_Name']} does not exist")
+            
+            # Check if item is being used in a transaction
+            try : 
+                cur.execute("DELETE FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            except psycopg2.errors.ForeignKeyViolation as e:
+                return (f"Item with name {data['Item_Name']} cannot be deleted as it is being used in a transaction")
+            
+            return (f"Item with name {data['Item_Name']} deleted successfully")
+
+# Update item quantity
+@products_bp.post('/update_quantity')
+def update_item():
+    data = request.json
+    with conn:
+        with conn.cursor() as cur:
+            
+            # Check if item exists
+            cur.execute("SELECT * FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            item = cur.fetchall()
+            if len(item) == 0:
+                return (f"Item with name {data['Item_Name']} does not exist")
+            
+            # Update item quantity
+            cur.execute("UPDATE InventoryItem SET Item_Qty = %s WHERE Item_Name = %s;", (data['Item_Qty'], data['Item_Name']))
+            return (f"Item with name {data['Item_Name']} updated successfully")
+        
+# Update item price
+@products_bp.post('/update_price')
+def update_price():
+    data = request.json
+    with conn:
+        with conn.cursor() as cur:
+            
+            # Check if item exists
+            cur.execute("SELECT * FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            item = cur.fetchall()
+            if len(item) == 0:
+                return (f"Item with name {data['Item_Name']} does not exist")
+            
+            # Update item price
+            cur.execute("UPDATE InventoryItem SET Item_Price = %s WHERE Item_Name = %s;", (data['Item_Price'], data['Item_Name']))
+            return (f"Item with name {data['Item_Name']} updated successfully")
+    
+# Get item details
+@products_bp.post('/get_item')
+def get_item():
+    data = request.json
+    with conn:
+        with conn.cursor() as cur:
+            
+            # Check if item exists
+            cur.execute("SELECT * FROM InventoryItem WHERE Item_Name = %s;", (data['Item_Name'],))
+            item = cur.fetchall()
+            if len(item) == 0:
+                return (f"Item with name {data['Item_Name']} does not exist")
+            
+            # Return item details
+            return (f"Item Name: {item[0][1]}<br>Item Description: {item[0][2]}<br>Item Price: {item[0][3]}<br>Item Quantity: {item[0][4]}")
